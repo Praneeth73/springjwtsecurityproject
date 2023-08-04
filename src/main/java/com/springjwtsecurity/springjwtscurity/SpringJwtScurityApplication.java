3d3416af7dd5b0bbfc34bc2d29548e47a2947749
praneethgunda <praneethsai73@gmail.com>
2023-08-03 20:41:36 -0600

    First commit for the jwt changes

---
+++ src/main/java/com/springjwtsecurity/springjwtscurity/SpringJwtScurityApplication.java
@@ -0,0 +1,13 @@
+package com.springjwtsecurity.springjwtscurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtScurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtScurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springjwtsecurity/springjwtscurity/SpringJwtScurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjwtsecurity.springjwtscurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtScurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
