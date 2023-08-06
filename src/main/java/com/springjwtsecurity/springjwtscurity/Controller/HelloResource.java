c25c2018a5a21ec847bf1f43aeae6d2d3bf8004a
praneethgunda <praneethsai73@gmail.com>
2023-08-05 19:34:48 -0600

    Corrected class

--- src/main/java/com/springjwtsecurity/springjwtscurity/Controller/HelloResource.java
+++ src/main/java/com/springjwtsecurity/springjwtscurity/Controller/HelloResource.java
@@ -9,14 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloResoure {
+public class HelloResource {
 
     @Autowired
     public AuthenticationManager authenticationManager;
@@ -29,7 +28,6 @@ public class HelloResoure {
 
     @GetMapping("/hello")
     public ResponseEntity<String> Hello(){
-        System.out.println("Hello");
         String re = "Val";
         return  ResponseEntity.ok().body(re);
     }
